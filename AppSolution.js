81c46f05297a4e7cd3376da1f78381a706006e82
BugHunter-X <askzenai@gmail.com>
2025-01-07 00:44:06 +0100

    Add AppSolution.js file

---
+++ AppSolution.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* The catch-all route must be last */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {return <div>Home</div>}
+function About() {return <div>About</div>}
+function NotFound() { return <div>404 Not Found</div>}
